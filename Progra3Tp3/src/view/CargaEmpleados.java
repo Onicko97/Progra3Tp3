e0148c8f74fa4e4981ba29187ed25a531959f2be
fogel98 <vfogel98@gmail.com>
2026-05-17 20:34:13 -0300

    nueva ventana de carga de empleados

--- Progra3Tp3/src/view/CargaEmpleados.java
+++ Progra3Tp3/src/view/CargaEmpleados.java
@@ -3,24 +3,29 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Empleados extends JDialog {
+public class CargaEmpleados extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textFieldNombre;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		try {
-			Empleados dialog = new Empleados();
+			CargaEmpleados dialog = new CargaEmpleados();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -31,14 +36,42 @@ public class Empleados extends JDialog {
 	/**
 	 * Create the dialog.
 	 */
-	public Empleados() {
-		setTitle("Empleados");
+	public CargaEmpleados() {
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setTitle("Carga de Empleados");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(40, 40, 67, 14);
+		contentPanel.add(lblNombre);
+		
+		textFieldNombre = new JTextField();
+		textFieldNombre.setBounds(146, 37, 200, 20);
+		contentPanel.add(textFieldNombre);
+		textFieldNombre.setColumns(10);
+		
+		JLabel lblRol = new JLabel("Rol:");
+		lblRol.setBounds(40, 80, 46, 14);
+		contentPanel.add(lblRol);
+		
+		JComboBox comboBoxRol = new JComboBox();
+		comboBoxRol.setBounds(146, 76, 200, 22);
+		contentPanel.add(comboBoxRol);
+		comboBoxRol.setModel(new DefaultComboBoxModel(new String[] {"Seleccione un rol","Lider de proyecto","Arquitecto","Programador","Tester"}));
+		
+		JLabel lblCalificacion = new JLabel("Calificacion:");
+		lblCalificacion.setBounds(40, 120, 82, 14);
+		contentPanel.add(lblCalificacion);
+		
+		JComboBox comboBoxCalificacion = new JComboBox();
+		comboBoxCalificacion.setBounds(146, 116, 200, 22);
+		contentPanel.add(comboBoxCalificacion);
+		comboBoxCalificacion.setModel(new DefaultComboBoxModel(new String[] {"Seleccione una calificacion","1","2","3","4","5"}));
+		
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -67,5 +100,4 @@ public class Empleados extends JDialog {
 			}
 		}
 	}
-
 }
